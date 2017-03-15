5690802dbbbf54b58ff68769c5103d6184864626
computereng <worawuth.kab@siam.edu>
2017-03-15 14:52:24 +0700

    Update weatherapi.php

--- weatherapi.php
+++ weatherapi.php
@@ -1,5 +1,5 @@
 <?php
-$json_string = file_get_contents("http://api.wunderground.com/api/aded58d2c14cec26/geolookup/conditions/q/TH/Bangkok.json");
+$json_string = file_get_contents("http://api.wunderground.com/api/aded58d2c14cec26/forecast10day/q/TH/Bangkok.json");
   $parsed_json = json_decode($json_string);
   $local_time_rfc822 = $parsed_json->{'current_observation'}->{'local_time_rfc822'};
   $location = $parsed_json->{'location'}->{'city'};
