eec4f5b909b9505fd37fd8aef0f222137c488c17
computereng <worawuth.kab@siam.edu>
2017-03-15 15:52:22 +0700

    Update weatherapi.php

--- weatherapi.php
+++ weatherapi.php
@@ -2,7 +2,7 @@
 $json_string = file_get_contents("http://api.wunderground.com/api/aded58d2c14cec26/forecast10day/q/TH/Bangkok.json");
   $parsed_json = json_decode($json_string);
   $data = $parsed_json->{'forecast'}->{'txt_forecast'}->{'forecastday'};
-  print_r($data);
+ // print_r($data);
   //$local_date = $parsed_json->{'forecast'}->{'txt_forecast'};
 foreach($data['forecastday'] as $period => $value) {
   echo "Max temperature for day " . $value[title] . " will be " . $value[fcttext_metric] . "<br />" ;
