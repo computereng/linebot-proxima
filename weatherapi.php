544ef8a4fb3620ce5d758fb406ba3541aba295dd
computereng <worawuth.kab@siam.edu>
2017-03-15 16:06:31 +0700

    Update weatherapi.php

--- weatherapi.php
+++ weatherapi.php
@@ -4,9 +4,9 @@ $json_string = file_get_contents("http://api.wunderground.com/api/aded58d2c14cec
   $data = $parsed_json->{'forecast'}->{'txt_forecast'}->{'forecastday'};
   print_r($data);
   //$local_date = $parsed_json->{'forecast'}->{'txt_forecast'};
-//foreach($data['txt_forecast'] as $forecastday => $value) {
-  //echo "Current temperature in " . $value[title] . " will be " . $value[fcttext_metric] . "<br />" ;
+foreach($data['forecastday']) {
+  echo "Current temperature in " . $title . " will be " . $fcttext_metric . "<br />" ;
   //$location = $parsed_json->{'location'}->{'city'};
   //$temp_c = $parsed_json->{'current_observation'}->{'temp_c'};
   //echo "Current temperature in $forecastday['title'][";
-//};
+};
