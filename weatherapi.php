cd8972b496d3dd4af52c6566ec906fdf9234a0c7
computereng <worawuth.kab@siam.edu>
2017-03-16 00:36:53 +0700

    Update weatherapi.php

--- weatherapi.php
+++ weatherapi.php
@@ -1,16 +1,21 @@
 <?php
-  $json_string = file_get_contents("http://api.wunderground.com/api/aded58d2c14cec26/geolookup/conditions/q/TH/Bangkok.json");
+$dd = 15;
+$cdd = 1;
+for($cdd <= 5; $cdd++){
+  $json_string = file_get_contents("http://api.wunderground.com/api/a6be6269233f1bc8/history_201703".$dd."/q/TH/Bangkok.json");
   $parsed_json = json_decode($json_string);
   $location = $parsed_json->{'location'}->{'city'};
   $temp_c = $parsed_json->{'current_observation'}->{'temp_c'};
   $local_time_rfc822 = $parsed_json->{'current_observation'}->{'local_time_rfc822'};
-  echo "Sussecc Current temperature in ${location} is: ${temp_c} on: ${local_time_rfc822}\n "; 
+  echo "Sussecc Current temperature in ${location} is: ${temp_c} on: ${dd}\n ";
+  $dd--;
+}
 
-  $db = pg_connect ("host=ec2-54-235-173-161.compute-1.amazonaws.com port=5432 dbname=davg135f89ndd9 user=nssvhpjghqgfui password=b32e79395f1d23198ce048097efca1604cf445e0fcabac8ae48f95b15ace1d81");
-/*if (!$db) {
+/* $db = pg_connect ("host=ec2-54-235-173-161.compute-1.amazonaws.com port=5432 dbname=davg135f89ndd9 user=nssvhpjghqgfui password=b32e79395f1d23198ce048097efca1604cf445e0fcabac8ae48f95b15ace1d81");
+if (!$db) {
     die('Could not connect: ' . mysql_error());
 }
-echo 'Connected successfully';*/
+echo 'Connected successfully';
 mysql_close($link);  
 $date = pg_escape_string($_POST['local_time_rfc822']); 
   $temp = pg_escape_string($_POST['temp_c']); 
@@ -22,7 +27,7 @@ $date = pg_escape_string($_POST['local_time_rfc822']);
             echo "Error with query: " . $errormessage; 
             exit(); 
    }  
-  pg_close(); 
+  pg_close(); */
   
 ///
  
