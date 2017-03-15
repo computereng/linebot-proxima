baf042126514f1d6127cb1a633e86afcce03cef3
computereng <worawuth.kab@siam.edu>
2017-03-15 14:46:27 +0700

    Update weatherapi.php

--- weatherapi.php
+++ weatherapi.php
@@ -1,6 +1,7 @@
 <?php
 $json_string = file_get_contents("http://api.wunderground.com/api/aded58d2c14cec26/geolookup/conditions/q/TH/Bangkok.json");
   $parsed_json = json_decode($json_string);
+  $local_time_rfc822 = $parsed_json->{'location'}->{'local_time_rfc822'};
   $location = $parsed_json->{'location'}->{'city'};
-  $temp_f = $parsed_json->{'current_observation'}->{'temp_f'};
-  echo "Current temperature in ${location} is: ${temp_f}\n";
+  $temp_c = $parsed_json->{'current_observation'}->{'temp_c'};
+  echo "Current temperature in ${location} is: ${temp_c} on ${local_time_rfc822}";
