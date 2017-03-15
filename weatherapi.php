0a1a4bdabbbd1ec7bde8129c9f852a85ccc44834
computereng <worawuth.kab@siam.edu>
2017-03-15 18:07:59 +0700

    Update weatherapi.php

--- weatherapi.php
+++ weatherapi.php
@@ -4,9 +4,15 @@
   $location = $parsed_json->{'location'}->{'city'};
   $temp_c = $parsed_json->{'current_observation'}->{'temp_c'};
   $local_time_rfc822 = $parsed_json->{'current_observation'}->{'local_time_rfc822'};
- 
-  $db = pg_connect('host=ec2-54-235-173-161.compute-1.amazonaws.com port=5432 dbname=davg135f89ndd9 user=nssvhpjghqgfui password=b32e79395f1d23198ce048097efca1604cf445e0fcabac8ae48f95b15ace1d81');
-  $date = pg_escape_string($_POST['local_time_rfc822']); 
+  echo "Sussecc Current temperature in ${location} is: ${temp_c} on: ${local_time_rfc822}\n "; 
+
+  $db = pg_connect ("host=ec2-54-235-173-161.compute-1.amazonaws.com port=5432 dbname=davg135f89ndd9 user=nssvhpjghqgfui password=b32e79395f1d23198ce048097efca1604cf445e0fcabac8ae48f95b15ace1d81");
+if (!$db) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+/*mysql_close($link);  
+$date = pg_escape_string($_POST['local_time_rfc822']); 
   $temp = pg_escape_string($_POST['temp_c']); 
   $location = pg_escape_string($_POST['location']);
   $query = ("INSERT INTO weather VALUES('$date', '$temp', '$location')");
@@ -16,8 +22,8 @@
             echo "Error with query: " . $errormessage; 
             exit(); 
    }  
-  pg_close(); 
-  echo "Sussecc Current temperature in ${location} is: ${temp_c} on: ${local_time_rfc822}\n ";
+  pg_close(); */
+  
 ///
  
 ?>
