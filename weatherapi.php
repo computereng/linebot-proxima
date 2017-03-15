7ad64510f083be133b09eff2a8d022f61b3ad3c8
computereng <worawuth.kab@siam.edu>
2017-03-16 01:00:07 +0700

    Update weatherapi.php

--- weatherapi.php
+++ weatherapi.php
@@ -7,8 +7,7 @@ for($cdd = 1; $cdd <= 5; $cdd++){
   $pretty = $parsed_json->{'history'}->{'observations'};
   //$temp_c = $parsed_json->{'current_'}->{'temp_c'};
   //$local_time_rfc822 = $parsed_json->{'current_observation'}->{'local_time_rfc822'};
-  $out = var_dump($pretty);
-  echo = $out;
+  var_dump($pretty[1]);
   echo $dd."\n";
   $dd--;
 }
