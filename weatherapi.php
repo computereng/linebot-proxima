cece7bfe28db651e122c925ffff494b9afd63182
computereng <worawuth.kab@siam.edu>
2017-03-15 16:54:10 +0700

    Update weatherapi.php

--- weatherapi.php
+++ weatherapi.php
@@ -1,13 +1,9 @@
 <?php
-$json_string = file_get_contents("http://api.wunderground.com/api/aded58d2c14cec26/forecast10day/q/TH/Bangkok.json");
-  $parsed_json = json_decode($json_string);
-  $data = $parsed_json->{'forecast'}->{'txt_forecast'}->{'forecastday'};
-  $check = $parsed_json->{'forecast'}->{'txt_forecast'}->{'forecastday'}->{'period'};
-print_r($data['period']);
-  //$local_date = $parsed_json->{'forecast'}->{'txt_forecast'};
-//foreach($data['period'] as $data[]) {
-  //echo $period ;
-  //$location = $parsed_json->{'location'}->{'city'};
-  //$temp_c = $parsed_json->{'current_observation'}->{'temp_c'};
-  //echo "Current temperature in $forecastday['title'][";
-//};
+$city="Bangkok";
+$country="TH"; 
+$url="http://api.openweathermap.org/data/2.5/forecast/daily?q=".$city.",".$country."&units=metric&cnt=7&lang=en&appid=c0c4a4b4047b97ebc5948ac9c48c0559";
+$json=file_get_contents($url);
+$data=json_decode($json,true);
+$data['city']['name'];
+?>
+
