77be20d45ef9570617cd6d92fb6bb87fe9e4e02b
computereng <worawuth.kab@siam.edu>
2017-03-15 17:58:17 +0700

    Update weatherapi.php

--- weatherapi.php
+++ weatherapi.php
@@ -6,11 +6,10 @@
   $local_time_rfc822 = $parsed_json->{'current_observation'}->{'local_time_rfc822'};
  
   $db = pg_connect('host=ec2-54-235-173-161.compute-1.amazonaws.com port=5432 dbname=davg135f89ndd9 user=nssvhpjghqgfui password=b32e79395f1d23198ce048097efca1604cf445e0fcabac8ae48f95b15ace1d81');
-  $conn = pg_connect($db);
   $date = pg_escape_string($_POST['local_time_rfc822']); 
   $temp = pg_escape_string($_POST['temp_c']); 
   $location = pg_escape_string($_POST['location']);
-  $query = 'INSERT INTO weather VALUES('$date', '$temp', '$location')';
+  $query = ("INSERT INTO weather VALUES('$date', '$temp', '$location')");
   $result = pg_query($query);
   if (!$result) { 
             $errormessage = pg_last_error(); 
