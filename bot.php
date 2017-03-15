de151b7c302ec787352ae301dd46a1f091e9b750
computereng <worawuth.kab@siam.edu>
2017-03-15 18:00:59 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -16,11 +16,13 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
+			if ($text == "เหนื่อยไหม"){
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => "สภาพอากาศวันนี้"
 			];
+			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
