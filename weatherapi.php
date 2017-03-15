be5a7d446f1351e691d9b795c73d39da92cc8058
computereng <worawuth.kab@siam.edu>
2017-03-16 02:37:13 +0700

    Update weatherapi.php

--- weatherapi.php
+++ weatherapi.php
@@ -12,23 +12,24 @@ for($cdd = 1; $cdd <= 5; $cdd++){
   $dd--;
 }
 
-/* $db = pg_connect ("host=ec2-54-235-173-161.compute-1.amazonaws.com port=5432 dbname=davg135f89ndd9 user=nssvhpjghqgfui password=b32e79395f1d23198ce048097efca1604cf445e0fcabac8ae48f95b15ace1d81");
+ $db = pg_connect ("host=ec2-54-235-173-161.compute-1.amazonaws.com port=5432 dbname=davg135f89ndd9 user=nssvhpjghqgfui password=b32e79395f1d23198ce048097efca1604cf445e0fcabac8ae48f95b15ace1d81");
 if (!$db) {
     die('Could not connect: ' . mysql_error());
 }
-echo 'Connected successfully';
 mysql_close($link);  
-$date = pg_escape_string($_POST['local_time_rfc822']); 
-  $temp = pg_escape_string($_POST['temp_c']); 
-  $location = pg_escape_string($_POST['location']);
-  $query = ("INSERT INTO weather_api VALUES('$date', '$temp', '$location')");
+  $pushdate = pg_escape_string($_POST['date']); 
+  $pushmaxtemp = pg_escape_string($_POST['maxtempm']); 
+  $pushmintemp = pg_escape_string($_POST['mintempm']); 
+  $pushlocation = pg_escape_string($_POST['tzname']);
+  $query = ("INSERT INTO weather_proxima VALUES('$pushdate', '$pushmaxtemp', '$pushmintemp', '$pushlocation')");
   $result = pg_query($query);
   if (!$result) { 
             $errormessage = pg_last_error(); 
             echo "Error with query: " . $errormessage; 
             exit(); 
-   }  
-  pg_close(); */
+   } 
+  echo 'Send Data successfully';
+  pg_close(); 
   
 ///
  
