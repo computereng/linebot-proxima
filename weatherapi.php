b6438563d64fd0341d2b76487a2c05b2ca0898b2
computereng <worawuth.kab@siam.edu>
2017-03-15 17:24:22 +0700

    Update weatherapi.php

--- weatherapi.php
+++ weatherapi.php
@@ -1,12 +1,9 @@
 <?php
-  $json_string = file_get_contents("http://api.wunderground.com/api/aded58d2c14cec26/forecast10day/q/TH/Bangkok.json");
+  $json_string = file_get_contents("http://api.wunderground.com/api/aded58d2c14cec26/geolookup/conditions/q/TH/Bangkok.json");
   $parsed_json = json_decode($json_string);
-  $location = $parsed_json->{'forecast'}->{'txt_forecast'}->{'forecastday'};
-  $period = $location->{'period'};
-  echo ($location);
-//$output1 = $parsed_json->{'forecast'}->{'txt_forecast'}->{'forecastday'}->{'period'} ;
-  //$temp_f = $parsed_json->{'current_observation'}->{'temp_f'};
-//foreach ($location as $period) {
-  //echo "1;
+  $location = $parsed_json->{'location'}->{'city'};
+  $temp_f = $parsed_json->{'current_observation'}->{'temp_f'};
+  $local_time_rfc822 = $parsed_json->{'location'}->{'local_time_rfc822'};
+  echo "Current temperature in ${location} is: ${temp_f} on: ${local_time_rfc822}\n ";
 ?>
 
