b3b1a5bd1cd4522737383d4292575add0fd9c202
computereng <worawuth.kab@siam.edu>
2017-03-16 02:07:26 +0700

    Update weatherapi.php

--- weatherapi.php
+++ weatherapi.php
@@ -4,10 +4,10 @@ $dd = 15;
 for($cdd = 1; $cdd <= 5; $cdd++){
   $json_string = file_get_contents("http://api.wunderground.com/api/a6be6269233f1bc8/history_201703".$dd."/q/TH/Bangkok.json");
   $parsed_json = json_decode($json_string);
-  $date = $parsed_json->{'dailysummary'}->{'date'}->{'date'};
-  $tzname = $parsed_json->{'dailysummary'}->{'date'}->{'tzname'};
-  $maxtempm = $parsed_json->{'dailysummary'}->{'date'}->{'maxtempm'};
-  $mintempm = $parsed_json->{'dailysummary'}->{'date'}->{'mintempm'};
+  $date = $parsed_json->{'history'}->{'dailysummary'}->{'date'}->{'date'};
+  $tzname = $parsed_json->{'history'}->{'dailysummary'}->{'date'}->{'tzname'};
+  $maxtempm = $parsed_json->{'history'}->{'dailysummary'}->{'date'}->{'maxtempm'};
+  $mintempm = $parsed_json->{'history'}->{'dailysummary'}->{'date'}->{'mintempm'};
   echo "Current temperature in ${tzname} maxtemp is: ${maxtempm} mintemp is: ${mintempm} on: ${tzname}\n";
   $dd--;
 }
