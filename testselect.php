82b42e3d633ad707f023bff9c448772e3c737d55
computereng <worawuth.kab@siam.edu>
2017-03-16 16:01:40 +0700

    Update testselect.php

--- testselect.php
+++ testselect.php
@@ -9,10 +9,11 @@ $db = pg_connect ("postgres://flghpbnnuhfevu:835ecb49bf0c74bc09716dbecdd8aa5df0f
             exit(); 
         } 
             while($myrow = pg_fetch_assoc($result)) { 
-              $output = "Weather on : ".$myrow['date']."<br>Temp is : ".$myrow['tempc']."<br>Weather is : ".$myrow['weather']."<br>Pressure is : ".$myrow['pressure'];
-            
+             // $output = "Weather on : ".$myrow['date']."<br>Temp is : ".$myrow['tempc']."<br>Weather is : ".$myrow['weather']."<br>Pressure is : ".$myrow['pressure'];
+            echo $myrow['date']. "  |  ".$myrow['tempc'];
         } 
-echo $output;
+        echo "=======================";
+//echo $output;
 //<td>%s</td><td>%s</td><td>%s</td>   , htmlspecialchars($myrow['tempc']), htmlspecialchars($myrow['weather']), htmlspecialchars($myrow['pressure'])
 pg_close();
 ?>
