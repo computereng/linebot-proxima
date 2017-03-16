5546a6bb0188572f6cdbddf7d0f79f5902018cfc
computereng <worawuth.kab@siam.edu>
2017-03-16 15:09:10 +0700

    Update testselect.php

--- testselect.php
+++ testselect.php
@@ -1,7 +1,12 @@
 <?php
 $db = pg_connect ("postgres://flghpbnnuhfevu:835ecb49bf0c74bc09716dbecdd8aa5df0ff7fa84bde3876dba031b27d632abf@ec2-75-101-142-182.compute-1.amazonaws.com:5432/d5mmu71c2lbm9o");
     //$selectfields = array("imgid" => "");
-    $records = pg_select($db,"mmsfiles");
-    print_r($records);
+     $query = "SELECT * FROM weather_botline_proxima"; 
+        $result = pg_query($query); 
+        if (!$result) { 
+            echo "Problem with query " . $query . "<br/>"; 
+            echo pg_last_error(); 
+            exit(); 
+        } 
 pg_close();
 ?>
