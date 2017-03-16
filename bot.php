4b054f9056d4e3ed4feafe4779316581959985b0
computereng <worawuth.kab@siam.edu>
2017-03-16 17:06:07 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,89 +1,98 @@
 <?php
+
+//Connect to Line API
 $access_token = 'LZArldUUHwHc6ROvqoAeGz5Kdft2ShdvagfCoiaoPaTpxqjvtA4ImaLk6hbkVguSX6pqlYaJFRB/pLt/q/Ct5w4chCz8hShgIVBOzZYuYM1YPHg8FJ0KS4G8GD3T0iFv7qAbmBvIfFYElhJ+MRgXtQdB04t89/1O/w1cDnyilFU=';
 
-//condb
+//Connect to Database
 $db = pg_connect ("postgres://flghpbnnuhfevu:835ecb49bf0c74bc09716dbecdd8aa5df0ff7fa84bde3876dba031b27d632abf@ec2-75-101-142-182.compute-1.amazonaws.com:5432/d5mmu71c2lbm9o");
 			//echo $resultsql;
 
 //
-// Get POST body content
+//Get data from line api
 $content = file_get_contents('php://input');
-// Parse JSON
+//Decode json to php
 $events = json_decode($content, true);
-// Validate parsed JSON data
 
 
+//ถ้ามีการรับค่าจาก line api
 if (!is_null($events['events'])) {
-	// Loop through each event
+	//วนลูปการทำงาน
 	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
+		// ตรวจสอบเงื่อนไขว่าค่าที่รับมาเป็นข้อความหรือไม่
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			
-			// Get text sent
+			// เก็บข้อความที่รับมาในตัวแปร text
 			$text = $event['message']['text'];
-			// Get replyToken
+			// เก็บค่า Token [ข้อมูลยืนยันตนของ line] ในตัวแปร replytoken
 			$replyToken = $event['replyToken'];
-
+			
+			//ถ้าข้อความที่ส่งมาคือ "คำสั่ง"
 			if ($text == "คำสั่ง"){
+				//เก็บข้อความไว้ในตัวแปร output
 				$output = "========คำสั่งทั้งหมด=========\n|    data     | เช็คสภาพอากาศปัจจุบัน\n|   history   | ดูประวัติการเช็คสภาพอากาศ\n|clearhistory| ล้างประวัติ\n========================";
 			}
+			//ถ้าข้อความที่ส่งมาคือ "data"
 			if ($text == "data"){
-			///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-				//select//
+			 //รับข้อมูล รูปแบบ Json จากเว็บพยากรอากาศ
 			 $json_string = file_get_contents("http://api.wunderground.com/api/a6be6269233f1bc8/conditions/astronomy/q/TH/Bangkok.json");
-  			 $parsed_json = json_decode($json_string);
+  			// Decode json
+			$parsed_json = json_decode($json_string);
+			//นำค่าจากข้อมูล Json ในเซ็ตของ current_observertion{local_time_rfc822} เก็บไว้ในตัวแปร อ่านค่าเซ็นได้จาก url ด้านบน
  			 $date = $parsed_json->{'current_observation'}->{'local_time_rfc822'};
 			 $temp_c = $parsed_json->{'current_observation'}->{'temp_c'};	
  			 $weather = $parsed_json->{'current_observation'}->{'weather'};
  			 $pressure = $parsed_json->{'current_observation'}->{'pressure_mb'};
 			 
+			//นำค่าพยาการอากาศที่ได้ เตรียมส่งขึ้น Database
 			$pushdate = pg_escape_string($date); 
 			$pushtemp = pg_escape_string($temp_c);
   			$pushweather = pg_escape_string($weather); 
   			$pushpressure = pg_escape_string($pressure); 
+			//นำข้อมูลที่จะเก็บไส้ใน Database ใส่่ในตัสแปร query
   			$query = ("INSERT INTO weather_botline_proxima VALUES('$pushdate', '$pushtemp', '$pushweather', $pushpressure,'','');");
-  			$result = pg_query($query);
+  			//ส่งค่าไปยัง Database โดยเก็บไว้ในตาราง weather_botline_proxima
+			$result = pg_query($query);
+				//เก็บข้อความไว้ในตัวแปร output
 				$output = "Weather on\n ${date} \n=======================\nTemp is: ${temp_c}C \nWeather is:  ${weather} \nPressure is :  ${pressure}\n======================= ";
 			}
 			if ($text == "history"){
-			///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-				//select//
+			//นำคำสั่งที่จะใช้เก็บไว้ในตัวแปร query
 			 $query = "SELECT * FROM weather_botline_proxima"; 
+			//ทำการดึงข้อมูลจาก Database ใน table weather_botline_proxima	
        			 $result = pg_query($query); 
-			if (!$result) { 
-           			echo "Problem with query " . $query . "<br/>"; 
-            			echo pg_last_error(); 
-            			exit(); 
-        		} 
-				$output = "  -:-History Get Weather-:-\n=======================\n";
-           		 while($myrow = pg_fetch_assoc($result)) { 
+			 $output = "  -:-History Get Weather-:-\n=======================\n";
+           		 //ทำการนำข้อมูลออกมา โดยเรียงจากแถว บนสุดลงล่าง
+			while($myrow = pg_fetch_assoc($result)) { 
+				//เก็บไว้ในตัวแปร output
               			$output = $output."Weather on : ".$myrow['date']."\nTemp is : ".$myrow['tempc']."\nWeather is : ".$myrow['weather']."\nPressure is : ".$myrow['pressure']."\n=======================\n";
        			 } 
 			}
 			if ($text == "clearhistory"){
-			///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-				//select//
+			
 			 $query = "DELETE FROM weather_botline_proxima"; 
+			//ทำการเคลียข้อมูลทั้งในใน table weather_botline_proxima
        			 $result = pg_query($query);
 				$output = "ทำการลบประวัติเรียบร้อยแล้ว";
 			}
-			
-			//////////
-			// Build message to reply back
+			//เตรียมข้อความที่จะส่งกลับไว้ในตัวแปร อาเรย์ messege
 			$messages = [
 				'type' => 'text',
 				'text' => $output
 			];
+			//ยกเลิกการ Connect Database
 			pg_close();
-			// Make a POST Request to Messaging API to reply to sender
+			//เก็บค่า link ของไลน์bot
 			$url = 'https://api.line.me/v2/bot/message/reply';
+			//รวมข้อมูลทั้งหมดไว้ใน อาเรย์ data เตรียมเข้ารหัสเป็น Json
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
 			];
+			//เข้ารหัสเป็น Json เพื่อเตรียมส่งกลับไปใน line
 			$post = json_encode($data);
+			//สร้าง Header ในการส่งสำหรับ line
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
+			// ทำการส่งค่าที่ทำการ เข้ารหัสเป็น Json ไปยังแชทไลน์
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
