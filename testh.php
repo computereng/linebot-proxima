6647993f2ffaaa170edd56a198ed54eb138fc338
computereng <worawuth.kab@siam.edu>
2017-03-17 02:42:28 +0700

    Update testh.php

--- testh.php
+++ testh.php
@@ -1,5 +1,6 @@
 <?php
 $html = new DOMDocument();
-$html->loadHTMLFile('status.html');
-echo $html;
+$html->loadHTMLFile('testh.html');
+$id2 = $html->getElementById('data_name2');
+print $id2->nodeValue . "\n";
 ?>
